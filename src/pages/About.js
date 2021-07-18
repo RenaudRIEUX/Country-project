045c07bfd83c3c662897481e6f02a56e82bef975
RenaudRIEUX <rieux.renaud.94@gmail.com>
2021-07-18 10:30:58 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+        <Navigation/>
+        <Logo />
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quasi?</p>
+       
+        </div>
+    );
+};
+
+export default About;
